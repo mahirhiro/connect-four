8aed5dc0ac3d4e6c26b4502257025ddf669e3ae3
theejazz <damianvanbeekum@gmail.com>
2019-08-05 20:49:22 +0200

    Removed name field from Checker---it is never used and likely never will?

--- src/main/java/connectFour/model/Checker.java
+++ src/main/java/connectFour/model/Checker.java
@@ -8,23 +8,19 @@ public class Checker extends Rectangle {
     private static int DEFAULT_Y = 450;
     private static final int DEFAULT_WIDTH = 60;
     private static final int DEFAULT_HEIGHT = 60;
-    private static final String DEFAULT_NAME = "Circle";
-    private String name;
     private static int shift_x = 0;
     private static int shift_y = 0;
 
-    public Checker(int x, int y, int width, int height, String name) {
+    public Checker(int x, int y, int width, int height) {
         super(x,y,width,height);
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.name = name;
-
     }
 
     public Checker(){
-        this(DEFAULT_X + shift_x, DEFAULT_Y + shift_y, DEFAULT_WIDTH, DEFAULT_HEIGHT, DEFAULT_NAME);
+        this(DEFAULT_X + shift_x, DEFAULT_Y + shift_y, DEFAULT_WIDTH, DEFAULT_HEIGHT);
         shift_y -= 40;
     }
 
